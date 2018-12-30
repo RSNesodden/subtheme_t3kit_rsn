7a92b42d8f219b61697ab315f44de9ad9551e600
mabolek <mathias@lilio.com>
2018-12-30 16:44:42 +0100

    [TASK] Commented out RTE config include

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,5 +3,5 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['subtheme_t3kit_template_default']
-= 'EXT:subtheme_t3kit_template/Resources/Private/Extensions/RTE/Default.yaml';
+//$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['subtheme_t3kit_template_default']
+//= 'EXT:subtheme_t3kit_template/Resources/Private/Extensions/RTE/Default.yaml';
